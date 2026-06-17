195903c1392834f091ba88e144037369442ce72c
Ammar Khocan <ammarkhocan02@gmail.com>
2026-06-17 21:50:57 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello cek</h1>
+      <Button>hai</Button>
+    </div>
+  );
+}
+
+export default App;
