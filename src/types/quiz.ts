4b0e39e591d056cab7def1da3b4b19bf1de1a8cd
Ammar Khocan <ammarkhocan02@gmail.com>
2026-06-18 13:53:52 +0700

    feat: add QuizQuestion and Answer interfaces

---
+++ src/types/quiz.ts
@@ -0,0 +1,15 @@
+export interface QuizQuestion {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
+
+export interface Answer {
+  question: string;
+  selectedAnswer: string;
+  correctAnswer: string;
+  isCorrect: boolean;
+}
